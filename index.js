51773efc2f2d0fea008559bd95b6c18d25152370
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-07 21:16:01 -0400

    add pseudo code to calculate function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const calculate = (arr) => {
-  return arr
+  //it takes an array as a parameter
+
+  //it filters the array and validates whether each input is a number or operand and returns NaN
+  //if the operand is missing
+
+  //it converts stringafied numbers to numbers
+
+  //it ignores non numeric values, empty strings, and undefined
+
+  //it returns only the desired values
+
+  //it calculates the operation from the remaining values and returns the answer
+
 }
 
 module.exports = calculate
