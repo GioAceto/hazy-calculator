c0cb4e6c8043c1e3349b694b2480ce0fbcb54b83
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-08 19:09:48 -0400

    add filter method

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const calculate = (arr) => {
-  //it takes an array as a parameter
+  // it takes an array as a parameter
 
-  //it filters the array and validates whether each input is a number or operand and returns NaN
-  //if the operand is missing
+  // it filters the array and validates whether each input is a number or operand and returns NaN
+  // if the operand is missing
+  const filteredArray = arr.filter((value) => {
+    return value
+  })
+  // it converts stringafied numbers to numbers
 
-  //it converts stringafied numbers to numbers
+  // it ignores non numeric values, empty strings, and undefined
 
-  //it ignores non numeric values, empty strings, and undefined
+  // it returns only the desired values
 
-  //it returns only the desired values
-
-  //it calculates the operation from the remaining values and returns the answer
-
+  // it calculates the operation from the remaining values and returns the answer
 }
 
 module.exports = calculate
