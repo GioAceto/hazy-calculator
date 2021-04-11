1eaa4e98e21b4bbc8151397b883a39222d59e4e1
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-10 20:26:35 -0400

    change function to use switch statements

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
+
 const calculate = (arr) => {
   let result = NaN
 
-  const filteredArray = arr.filter((value) => {
-    if ((value == parseInt(value) ||
-      value === '+' ||
-      value === '-' ||
-      value === '*' ||
-      value === '/') &&
-      value !== '' ||
-      undefined) {
-      return value
-    }
-  })
+  const filteredArray = (arr) => {
+    let newArray = arr.filter(value => {
+      return goodValues(value)
+    })
 
-
-  if (filteredArray[1] === '+') {
-    result = filteredArray[0] + filteredArray[2]
-  } else if (filteredArray[1] === '-') {
-    result = filteredArray[0] - filteredArray[2]
-  } else if (filteredArray[1] === '*') {
-    result = filteredArray[0] * filteredArray[2]
-  } else if (filteredArray[1] === '/') {
-    result = filteredArray[0] / filteredArray[2]
+    return newArray
   }
+
+  switch ((resultArray[1])) {
+    case '+':
+      result = resultArray[0] + resultArray[2]
+      break
+    case '-':
+      result = resultArray[0] - resultArray[2]
+      break
+    case '*':
+      result = resultArray[0] * resultArray[2]
+      break
+    case '/':
+      result = resultArray[0] / resultArray[2]
+      break
+  }
+
   return result
 }
+
+const goodValues = (value) => {
+  if (value == Number(value)) {
+    return true
+  } else {
+    switch (value) {
+      case Number(value):
+      case null:
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        return true
+      default:
+        return false
+    }
+  }
+}
+
 
 module.exports = calculate
