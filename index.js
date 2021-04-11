d9151d4fc4ad3c47b6cbba392d31032429726a03
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-11 05:39:05 -0400

    refactored switch statement in index file with variables num and operand

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const goodValues = require('./good-values')
 
 const calculate = (arr) => {
   let result = NaN
-
   let resultArray = arr.filter(value => goodValues(value))
+  let num1 = resultArray[0]
+  let operand = resultArray[1]
+  let num2 = resultArray[2]
 
   switch ((resultArray[1])) {
     case '+':
-      result = resultArray[0] + resultArray[2]
+      result = num1 + num2
       break
     case '-':
-      result = resultArray[0] - resultArray[2]
+      result = num1 - num2
       break
     case '*':
-      result = resultArray[0] * resultArray[2]
+      result = num1 * num2
       break
     case '/':
-      result = resultArray[0] / resultArray[2]
+      result = num1 / num2
       break
   }
 
